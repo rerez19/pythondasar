fc0e09642f0f5e5e68e2b4f08989714b4adfac8b
rezky1912 <umburezky@gmail.com>
2024-05-06 13:52:27 +0700

    loop

---
+++ loopexample.py
@@ -0,0 +1,21 @@
+#for loop
+for i in range(5):
+    print(i)
+
+#while loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+#break
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+#continue
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
