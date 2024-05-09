8da40424ab9d783168d8743fc3de955e4a39753a
rezky1912 <umburezky@gmail.com>
2024-05-09 12:47:35 +0700

    update bmi

--- program dasar/bmicalc.py
+++ program dasar/bmicalc.py
@@ -4,11 +4,22 @@ def calculate_bmi(weight_kg, height_m):
     bmi = weight_kg / (height_m ** 2)
     return bmi
 
+def bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif bmi < 25:
+        return "Normal weight"
+    elif bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
 
-weights = np.array([70, 80, 90])  
-heights = np.array([1.75, 1.8, 1.65])
-
+weights = np.array([70, 80, 90]) 
+heights = np.array([1.75, 1.8, 1.65]) 
 
 bmis = calculate_bmi(weights, heights)
 
-print("BMI for each person:", bmis)
+categories = np.array([bmi_category(bmi) for bmi in bmis])
+
+for i in range(len(weights)):
+    print("Person", i+1, ":", "BMI:", bmis[i], ",", "Category:", categories[i])
